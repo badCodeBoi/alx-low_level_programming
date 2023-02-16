67d37e93bab25147a3ca2727f1ae75b2df9c5893
badCodeBoi <tumitaunyane@icloud.com>
2023-02-16 10:06:07 -0800

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * above is the command that includes all needed resources 
+ * main - main function with code to print out a line 
+ * puts - this is a function in main,that allows main to print out the desired line
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
