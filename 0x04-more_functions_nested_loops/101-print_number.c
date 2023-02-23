df3e7d688ca905eb77d155ff724c6855faceb1d9
badCodeBoi <tumitaunyane@icloud.com>
2023-02-23 10:44:18 -0800

    fix2

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - prints integers
+ *@n: the variable to print
+ * Return: non
+ */
+
+void print_number(int n)
+{
+unsigned int a = n;
+if (n < 0)
+{
+n == -1;
+a = n;
+_putchar('_');
+}
+a /= 10;
+print_number(k);
+_putchar((unsigned int) n % 10 + 0);
+}
