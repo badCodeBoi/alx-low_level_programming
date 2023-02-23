91a35cf46296482a1efb0bd4b632de6413b4da71
badCodeBoi <tumitaunyane@icloud.com>
2023-02-23 06:20:46 -0800

    merger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -
+ * @a: first integer to be multiplied 
+ * @b: second integer to be multiplied
+ * Return: the result of the calculation
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 0 to 9
+ *
+ *Return: 0-9 excluding 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+int n = 0;
+
+for (; n <= 9; n++)
+{
+if (n == 2 || n == 4)
+{
+continue;
+}
+else
+{
+_putchar(n + '0');
+}
+}
+_putchar('\n');
+}
