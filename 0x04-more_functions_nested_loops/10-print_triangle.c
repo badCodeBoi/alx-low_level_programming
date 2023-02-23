3011332f342210794fc13e96c63e7b358c8aa309
badCodeBoi <tumitaunyane@icloud.com>
2023-02-23 16:17:33 +0200

    fix fizz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangles of squares
+ * @size: size of triangles
+ * Return: non
+ */
+
+void print_triangle(int size)
+{
+int x, y, a;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = size - x; y > 1; y--)
+{
+_putchar(32);
+}
+for (a = 0;a <= x; a++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.c"
+
+/**
+ * print_line - will draw a straight line in the terminal.
+ * @n: if 0 or less, the function should only print \n
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int x;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+}
