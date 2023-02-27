764e6271ffc729d9c48ab4c0d800f960127b3dd3
badCodeBoi <tumitaunyane@icloud.com>
2023-02-27 08:42:06 -0800

    first fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ void puts2(char *str);
 void rev_string(char *s);
 int _strlen(char *s);
 int _putchar(char c)
-void swap_int(int *a, int *b);void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void print_rev(char *s);
 char *_strcpy(char *dest, char *src);
