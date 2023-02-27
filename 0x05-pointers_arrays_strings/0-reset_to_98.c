c9ab2f19197c3680f287de4021cfdc0a726218a6
badCodeBoi <tumitaunyane@icloud.com>
2023-02-27 08:07:30 -0800

    first0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - uses pointer to update a values points to 98
+ * @n: input variable
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void _puts(char *str);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+int _strlen(char *s);
+int _putchar(char c)
+void swap_int(int *a, int *b);void reset_to_98(int *n);
+void reset_to_98(int *n);
+void print_rev(char *s);
+char *_strcpy(char *dest, char *src);
+
