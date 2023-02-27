eec481b2252199c13230a4aa2210af668f19d9f0
badCodeBoi <tumitaunyane@icloud.com>
2023-02-27 11:21:29 -0800

    fixing business

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - prints elements of an array of integers
+* @a: the array to be used
+* @n: variables to be printed
+*/
+
+void print_array(int *a, int n)
+{
+int t;
+for (t = 0; t < n; t++)
+{
+printf("%d", a[t]);
+if (t != n - 1)
+	printf(", ");
+}
+printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
 int _strlen(char *s);
-int _putchar(char c)
+int _putchar(char c);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void print_rev(char *s);
