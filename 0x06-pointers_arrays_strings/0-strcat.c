7da3079be1a7d85a2007c7ba0ed2b080cb3264be
badCodeBoi <tumitaunyane@icloud.com>
2023-03-01 15:38:40 +0200

    fix1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - concatenates two strings
+ * @src: string to append
+ * @dest: pointer to the resulting string
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int len = 0, i;
+while (dest[len])
+{
+len++;
+}
+for (i = 0; src[i] != 0; i++)
+{
+dest[len] = src[i];
+len++;
+}
+dest[len] = '\0';
+return (dest);
+}
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,12 +2,23 @@
 #include <stdio.h>
 
 /**
- * main - 
- * @src:
- * @dest:
- * Return: 0
+ * *_strncat - concatenates two strings
+ * @src: string to append
+ * @dest: pointer to the resulting string
+ * @n: numbers of bytes needed from src
+ * Return: a pointer to the resulting string dest
  */
-main()
+char *_strncat(char *dest, char *src, int n)
 {
-   
+int index = 0, dest_len = 0;
+
+while (dest[index++])
+{
+dest_len++;
+}
+for (index = 0; src[index] && index < n; index++)
+{
+dest[dest_len++] = src[index];
+}
+return (dest);
 }
